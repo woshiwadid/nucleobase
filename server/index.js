6687a0a47a2da38ba0ce2aaa21d37089b100deb2
gchoupeaux <gchoupeaux@gmail.com>
2017-08-31 19:46:08 -0700

    deploytest4

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 const app = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}!`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
 });
