d892fc4fe2674694a3831ced1d62d5f0ca96354b
gchoupeaux <gchoupeaux@gmail.com>
2017-09-02 11:04:32 -0700

    pipeline test

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const db = require('../db');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+  console.log(`listening on port ${port}!`);
 });
